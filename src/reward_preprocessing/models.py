459b50ba5f80613efdb8be70ae6e6a317f601c18
Pavel <pavelc.git@posteo.net>
2022-08-15 10:59:13 -0700

    Start implementing CNN regression rewards

--- src/reward_preprocessing/models.py
+++ src/reward_preprocessing/models.py
@@ -1,11 +1,30 @@
 from typing import Tuple
 
 import gym
+import torch as th
 from imitation.rewards.reward_nets import RewardNet
 import numpy as np
 import torch
 
 from reward_preprocessing.env import maze, mountain_car  # noqa: F401
+
+class CNNRegressionRewardNet(RewardNet):
+    """CNN for learning reward using supervised regression from trajectories."""
+
+    def __int__(self):
+
+    def forward(self, state: th.Tensor, action: th.Tensor, next_state: th.Tensor,
+                done: th.Tensor) -> th.Tensor:
+        """
+        Args:
+            state: Tensor of shape (batch_size, state_size)
+            action: Tensor of shape (batch_size, action_size)
+            next_state: Tensor of shape (batch_size, state_size)
+            done: Tensor of shape (batch_size,)
+        Returns:
+            Tensor of shape (batch_size,)
+        """
+
 
 
 class MazeRewardNet(RewardNet):
