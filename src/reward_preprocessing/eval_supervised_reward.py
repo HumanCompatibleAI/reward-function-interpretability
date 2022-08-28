c91d32059c88fde04be9b1c9de6b78a815c1c8b8
Pavel <pavelc.git@posteo.net>
2022-08-27 17:18:50 -0700

    Switfch to manual eval policy instead of using cli version

--- src/reward_preprocessing/eval_supervised_reward.py
+++ src/reward_preprocessing/eval_supervised_reward.py
@@ -1,23 +1,26 @@
 import logging
 import os.path as osp
-from typing import Optional
+from typing import Mapping, Optional
 
+from imitation.data import rollout
+from imitation.data.rollout import AnyPolicy
+from imitation.policies import serialize
+from imitation.rewards import reward_function
+from imitation.rewards.reward_wrapper import RewardVecEnvWrapper
 from imitation.rewards.serialize import (
     ValidateRewardFn,
     _make_functional,
+    load_reward,
     reward_registry,
 )
-from imitation.scripts import eval_policy
-
 from sacred import Experiment
 from sacred.observers import FileStorageObserver
+from stable_baselines3.common.vec_env import VecEnv
 import torch as th
 
-from reward_preprocessing.env import env_ingredient
+from reward_preprocessing.env import create_env, env_ingredient
 
-eval_supervised_ex = Experiment(
-    "eval_supervised", ingredients=[env_ingredient]
-)
+eval_supervised_ex = Experiment("eval_supervised", ingredients=[env_ingredient])
 
 # Register our custom reward so that imitation internals can e.g. load the specified
 # type. We register in a very similar way to imitation.reward.serialize
@@ -33,26 +36,32 @@ reward_registry.register(
 
 
 def _eval_policy(
-    eval_n_episodes: int,
-    policy_type: Optional[str],
-    reward_type: Optional[str],
-    policy_path: Optional[str] = None,
-    reward_path: Optional[str] = None,
-):
-    """Wrapper to make calling eval_policy more convenient."""
+    venv: VecEnv,
+    num_episodes: int = 100,
+    policy: AnyPolicy = None,
+    reward_fn: Optional[reward_function.RewardFn] = None,
+) -> Mapping[str, float]:
+    """Roll out a policy in an environment with optional reward function.
 
-    return eval_policy.eval_policy(
-        eval_n_episodes=eval_n_episodes,
-        policy_type=policy_type,
-        policy_path=policy_path,
-        reward_type=reward_type,
-        reward_path=reward_path,
-        eval_n_timesteps=None,
-        render=False,
-        render_fps=60,
-        videos=False,
-        video_kwargs={},
-    )
+    Args:
+        venv: The environment to roll out in.
+        num_episodes: Number of episodes to roll out.
+        policy: Optional policy to roll out. If None sample randomly.
+        reward_fn: Optional reward function to evaluate the policy with.
+    Returns:
+         Return value of `imitation.util.rollout.rollout_stats()`.
+    """
+    try:
+        if reward_fn is not None:
+            venv = RewardVecEnvWrapper(venv, reward_fn)
+        sample_until = rollout.make_sample_until(
+            min_episodes=num_episodes, min_timesteps=None
+        )
+        trajectories = rollout.generate_trajectories(policy, venv, sample_until)
+
+        return rollout.rollout_stats(trajectories)
+    finally:
+        venv.close()
 
 
 @eval_supervised_ex.config
@@ -70,69 +79,50 @@ def defaults():
 
 
 @eval_supervised_ex.main
-def main(
+def eval_reward(
     eval_n_episodes: int,
     policy_type: str,
     policy_path: Optional[str],
     reward_path: Optional[str],
 ):
-    if th.cuda.is_available():
-        device = th.device("cuda")
-    else:
-        device = th.device("cpu")
+    """Sanity check a learned supervised reward net. Evaluate 4 things:
+    - Random policy on env reward
+    - Random policy on learned reward function
+    - Expert policy on env reward
+    - Expert policy on learned reward function
+    """
+    venv = create_env()
 
-    # env = gym.make(f"procgen:procgen-{env_name}-v0", render_mode="rgb_array")
-    # env_max_steps = MaxEpisodeStepsWrapper(env, max_episode_steps=100)
-    # venv = DummyVecEnv([lambda: env])
-    # venv_max_steps = DummyVecEnv([lambda: env_max_steps])
+    rew_fn = load_reward("SupervisedRewardNet", reward_path, venv)
 
-    # reward_net = CoinReward()
-
-    # print("Eval before training")
-    # rollout_stats_before_training = eval_policy(venv_max_steps)
-    # print(rollout_stats_before_training)
+    policy = None
+    if policy_type is not None:
+        policy = serialize.load_policy(policy_type, policy_path, venv)
 
     logging.info(f"Evaluating random policy with env reward as sanity check.")
-    stats = _eval_policy(
-        eval_n_episodes=eval_n_episodes,
-        policy_type=None,
-        reward_type=None,
-    )
+    stats = _eval_policy(venv, num_episodes=eval_n_episodes)
     logging.info(stats)
 
     logging.info(f"Evaluating random policy on learned reward.")
-    stats = _eval_policy(
-        eval_n_episodes=eval_n_episodes,
-        policy_type=None,
-        reward_type="SupervisedRewardNet",
-        reward_path=reward_path,
-    )
+    stats = _eval_policy(venv, num_episodes=eval_n_episodes, reward_fn=rew_fn)
     logging.info(stats)
 
     logging.info(f"Evaluating expert policy on env reward.")
-    stats = _eval_policy(
-        eval_n_episodes=eval_n_episodes,
-        policy_type=policy_type,
-        policy_path=policy_path,
-        reward_type=None,
-    )
+    stats = _eval_policy(venv, num_episodes=eval_n_episodes, policy=policy)
     logging.info(stats)
 
     logging.info(f"Evaluating expert policy on learned reward.")
     stats = _eval_policy(
-        eval_n_episodes=eval_n_episodes,
-        policy_type=policy_type,
-        policy_path=policy_path,
-        reward_type="SupervisedRewardNet",
+        venv, num_episodes=eval_n_episodes, policy=policy, reward_fn=rew_fn
     )
     logging.info(stats)
 
 
-def main_console():
+def main():
     observer = FileStorageObserver(osp.join("output", "sacred", "eval_supervised"))
     eval_supervised_ex.observers.append(observer)
     eval_supervised_ex.run_commandline()
 
 
 if __name__ == "__main__":  # pragma: no cover
-    main_console()
+    main()
