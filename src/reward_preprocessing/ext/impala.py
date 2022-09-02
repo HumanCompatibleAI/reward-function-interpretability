cfd9e9f21ba538b50d96082af1d549041f06c682
Pavel <pavelc.git@posteo.net>
2022-09-02 16:33:22 -0700

    Add external impala model

---
+++ src/reward_preprocessing/ext/impala.py
@@ -0,0 +1,94 @@
+"""Excluded from LICENSE,
+source: https://github.com/joonleesky/train-procgen-pytorch/
+minor modifications, to make it an SB3 features extractor
+"""
+import gym
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+import torch.nn as nn
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+def xavier_uniform_init(module, gain=1.0):
+    if isinstance(module, nn.Linear) or isinstance(module, nn.Conv2d):
+        nn.init.xavier_uniform_(module.weight.data, gain)
+        nn.init.constant_(module.bias.data, 0)
+    return module
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        self.conv2 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+
+    def forward(self, x):
+        out = nn.ReLU()(x)
+        out = self.conv1(out)
+        out = nn.ReLU()(out)
+        out = self.conv2(out)
+        return out + x
+
+
+class ImpalaBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ImpalaBlock, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+        )
+        self.res1 = ResidualBlock(out_channels)
+        self.res2 = ResidualBlock(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)(x)
+        x = self.res1(x)
+        x = self.res2(x)
+        return x
+
+
+class ImpalaModel(BaseFeaturesExtractor):
+    def __init__(
+        self,
+        in_channels: int,
+        observation_space: gym.Space,
+        features_dim: int = 0,
+        **kwargs,
+    ):
+        super(ImpalaModel, self).__init__(observation_space, features_dim)
+        self.block1 = ImpalaBlock(in_channels=in_channels, out_channels=16)
+        self.block2 = ImpalaBlock(in_channels=16, out_channels=32)
+        self.block3 = ImpalaBlock(in_channels=32, out_channels=32)
+        self.fc = nn.Linear(in_features=32 * 8 * 8, out_features=features_dim)
+
+        # self.output_dim = 256
+        self.apply(xavier_uniform_init)
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = nn.ReLU()(x)
+        x = Flatten()(x)
+        x = self.fc(x)
+        x = nn.ReLU()(x)
+        return x
