fee12695afe9e70dc393e2e17c59ecdee90fc96f
Pavel <pavelc.git@posteo.net>
2022-09-03 22:08:41 -0700

    Add script for sb3 training

---
+++ src/reward_preprocessing/scripts/train_rl_sb3.py
@@ -0,0 +1,29 @@
+"""Quick ad-hoc script to train a RL agent using SB3."""
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import (
+    CallbackList,
+    CheckpointCallback,
+    EvalCallback,
+)
+
+save_freq = 1_000_000
+total_timesteps = 200_000_000
+
+checkpoint_callback = CheckpointCallback(
+    save_freq=save_freq, save_path="./out/sb3/checkpoints/"
+)
+eval_env = gym.make("procgen:procgen-coinrun-v0")
+eval_callback = EvalCallback(
+    eval_env,
+    log_path="./out/sb3/results",
+    eval_freq=save_freq,
+)
+# Create the callback list
+callback = CallbackList([checkpoint_callback, eval_callback])
+
+
+model = PPO("MlpPolicy", "procgen:procgen-coinrun-v0")
+model.learn(total_timesteps=total_timesteps, callback=callback)
+# Final save
+model.save("ppo_coinrun_v0")
