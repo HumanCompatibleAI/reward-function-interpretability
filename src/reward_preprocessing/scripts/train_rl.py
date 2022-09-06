8c8fead21b5a1024ae30578f68960d57fb89e545
Pavel <pavelc.git@posteo.net>
2022-09-06 14:29:16 -0700

    Update hparams from train procgen repo

--- src/reward_preprocessing/scripts/train_rl.py
+++ src/reward_preprocessing/scripts/train_rl.py
@@ -12,15 +12,19 @@ use_config(train_rl_ex)
 @train_rl_ex.named_config
 def coinrun_aisc_fixed_coin():
     """Coinrun with hparams from goal misgeneralization paper."""
+    # Hparams inferred from https://github.com/jbkjr/train-procgen-pytorch/blob/master/hyperparams/procgen/config.yml
+    # (hard-500)
     # This is the procgen version from procgenAISC, but the version that has the coin
     # always fixed at the end of the level, as in the original coinrun.
     train = dict(
         policy_cls=reward_preprocessing.policies.base.ImpalaPolicy,
         # policy_kwargs=dict(features_extractor_class=ImpalaModel),
     )
+    policy_save_interval = 1_000_000
     common = dict(
         env_name="procgen:procgen-coinrun-v0",
-        num_vec=64,  # Goal Misg paper uses 64 envs for each of 4 workers.
+        num_vec=256,  # Goal Misg paper uses 64 envs for each of 4 workers.
+        env_make_kwargs=dict(num_levels=100_000, distribution_mode="hard"),
     )
     total_timesteps = int(200_000_000)
     rl = dict(
@@ -29,10 +33,13 @@ def coinrun_aisc_fixed_coin():
             n_epochs=3,
             ent_coef=0.01,
             learning_rate=0.0005,
-            batch_size=2048,
+            batch_size=8192,
             gamma=0.999,
             gae_lambda=0.95,
             clip_range=0.2,
+            max_grad_norm=0.5,
+            vf_coef=0.5,
+            normalize_advantage=True,
         ),
     )
     # num_vec * n_steps / (mini)batch_size amounts to 8 minibatches as in the original
