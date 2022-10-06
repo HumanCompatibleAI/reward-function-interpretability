50333fb94bcda447bc9e93f9de8b2fa15c9eef05
Pavel <pavelc.git@posteo.net>
2022-10-06 12:32:53 +0200

    Fix loading reward nets when GPU is not available but was trained on GPU

--- src/reward_preprocessing/common/serialize.py
+++ src/reward_preprocessing/common/serialize.py
@@ -3,16 +3,25 @@ from imitation.rewards.serialize import load_reward as imitation_load_reward
 from imitation.rewards.serialize import reward_registry
 import torch as th
 
+
+def _loader_helper(path, _, **kwargs):
+    # AFAICT by default imitation will load saved reward nets and then torch will move
+    # them to the same devices they are saved on. This fails if e.g. GPU is not
+    # available in the environment that loads. The following circumvents this problem
+    # by using torch.load's map_location argument.
+    if th.cuda.is_available():
+        rew_net = th.load(str(path))  # Load from same device as saved
+    else:  # CUDA not available
+        rew_net = th.load(str(path), map_location=th.device("cpu"))  # Force CPU
+    return ValidateRewardFn(_make_functional(rew_net))
+
+
 # Register our custom reward so that imitation internals can e.g. load the specified
 # type. We register in a very similar way to imitation.reward.serialize
 reward_registry.register(
     key="SupervisedRewardNet",
     # Validate the shape returned by the reward net
-    value=lambda path, _, **kwargs: ValidateRewardFn(
-        _make_functional(  # Turns the loaded reward net into a reward function.
-            th.load(str(path)),
-        ),
-    ),
+    value=_loader_helper,
 )
 
 
