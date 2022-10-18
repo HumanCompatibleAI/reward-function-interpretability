a85d14c172d7ece35271508820f413c5d6536668
Pavel <pavelc.git@posteo.net>
2022-10-18 12:55:03 +0200

    Add rl vis utility 'show'

---
+++ src/reward_preprocessing/vis/util.py
@@ -0,0 +1,8 @@
+import scipy.ndimage as nd
+
+def zoom_to(img, width):
+    n = width // img.shape[-2] + 1
+    img = img.repeat(n, axis=-3).repeat(n, axis=-2)
+    r = float(width) / img.shape[-2]
+    zoom = [1] * (img.ndim - 3) + [r, r, 1]
+    return nd.zoom(img, zoom, order=0, mode="nearest")
