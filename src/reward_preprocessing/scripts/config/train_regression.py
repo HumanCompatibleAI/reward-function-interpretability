d08cd05afdc4762105f07358de9700bee570bb80
Pavel <pavelc.git@posteo.net>
2022-10-26 09:55:16 +0200

    Start adding named configs

--- src/reward_preprocessing/scripts/config/train_regression.py
+++ src/reward_preprocessing/scripts/config/train_regression.py
@@ -17,4 +17,28 @@ train_regression_ex = sacred.Experiment(
 def defaults():
     # Every checkpoint_epoch_interval epochs, save the model. Epochs start at 1.
     checkpoint_epoch_interval = 1
+    # Apparently in sacred I need default values, if I want to be able to override them
+    supervised = dict(net_kwargs=dict(hid_channels=(32, 64)))
+    locals()  # make flake8 happy
+
+
+@train_regression_ex.named_config
+def use_next_state():
+    supervised = dict(
+        net_kwargs=dict(use_state=False, use_action=False, use_next_state=True)
+    )
+    locals()  # make flake8 happy
+
+
+@train_regression_ex.named_config
+def use_all():
+    supervised = dict(
+        net_kwargs=dict(use_state=True, use_action=True, use_next_state=True)
+    )
+    locals()  # make flake8 happy
+
+
+@train_regression_ex.named_config
+def small():
+    supervised = dict(net_kwargs=dict(hid_channels=(32, 64)))
     locals()  # make flake8 happy
