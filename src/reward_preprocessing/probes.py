aa0d7c0b4b495030b23a5c6f0e5b2ad4cf6d613a
Daniel Filan <df@danielfilan.com>
2023-03-27 17:12:08 -0700

    Flesh out code for linear probes

--- src/reward_preprocessing/probes.py
+++ src/reward_preprocessing/probes.py
@@ -1,5 +1,6 @@
 """Train linear probes on reward nets."""
 
+import torch as th
 from torch import nn
 
 # TODO document anything
@@ -8,27 +9,28 @@ from torch import nn
 class Probe(nn.Module):
     # inspired by
     # https://github.com/yukimasano/linear-probes/blob/master/eval_linear_probes.py
-    def __init__(self, model, layer_name, attribute_dim):
+    # when you optimize, can just optimize over probe_head.params
+    def __init__(self, model: nn.Module, layer_name: str, attribute_dim: int):
         super(Probe, self).__init__()
         self.attribute_dim = attribute_dim
         self.model = model
         self.layer_name = layer_name
         self.probe_head = None
-        out_features = 0
-        for name, child in enumerate(model.named_children()):
+        x = th.zeros(1, 3, 64, 64)
+        for name, child in enumerate(self.model.named_children()):
+            x = child.forward(x)
             if name == self.layer_name:
                 avg_pool = nn.AdaptiveAvgPool2d(1)
                 flatten = nn.Flatten()
-                fc = nn.Linear(out_features, attribute_dim)
+                fc = nn.Linear(x.size(1), attribute_dim)
                 self.probe_head = nn.Sequential(avg_pool, flatten, fc)
-            if hasattr(child, "out_features"):
-                out_features = child.out_features
         if self.probe_head is None:
-            raise ValueError(f"Could not find layer {layer_name} to probe")
+            raise ValueError(f"Could not find layer {self.layer_name} to probe")
 
     def forward(self, x):
-        # go forward layer by layer thru self.model
-        # once we hit layer_name, apply probe_head to x, then output that
-        pass
+        for name, child in enumerate(self.model.named_children()):
+            x = child.forward(x)
+            if name == self.layer_name:
+                return self.probe_head(x)
+        assert False, f"Could not find layer {self.layer_name} to probe."
 
-    # then, when you optimize, can just optimize over probe_head.params
