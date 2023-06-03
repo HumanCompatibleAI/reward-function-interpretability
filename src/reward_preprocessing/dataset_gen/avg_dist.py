f7b8dc1ab301d172a32b5117a8ac14ffb737f6e8
Pavel <pavelc.git@posteo.net>
2023-06-03 18:15:43 +0200

    Save simple env obs as rollout

--- src/reward_preprocessing/dataset_gen/avg_dist.py
+++ src/reward_preprocessing/dataset_gen/avg_dist.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib
 
 matplotlib.use("Agg")
@@ -10,6 +12,8 @@ circle_radius = 0.05
 colors = ["r", "b", "g"]  # Add more colores to allow more pairs
 matplotlib.use("TkAgg")
 seed = 0
+num_transitions = 200
+traj_path = "/nas/ucb/pavel/rfi/test/simple-env-traj.npz"
 
 # Set the seed
 np.random.seed(seed)
@@ -57,10 +61,43 @@ def generate_transition():
     # fig.savefig("plotcircles.png", bbox_inches="tight")
     data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-    return data
+
+    plt.close()
+
+    avg_distance = np.mean(distances)
+    return data, avg_distance, distances
 
 
-data = generate_transition()
+obs_list = []
+infos_list = []
+avg_distances = []
+for i in range(num_transitions):
+    data, avg_distance, distances = generate_transition()
+    obs_list.append(data)
+    infos_list.append({"distances": distances})
+    avg_distances.append(avg_distance)
 
-plt.imshow(data, interpolation="nearest")
-plt.show()
+# Duplicate last observation, since there is always a final next_obs.
+obs_list.append(obs_list[-1].copy())
+
+condensed = {
+    "obs": obs_list,
+    "acts": np.zeros((num_transitions,)).astype(np.int8),
+    "infos": np.array(infos_list),
+    "terminal": np.array([True] * num_transitions),
+    "rews": np.array(avg_distances),
+    # The indices are pretty arbitrary, they show where an episode ends. Since we
+    # don't have a real RL environment, they are not really meaningful. As they are
+    # nevertheless required to save a rollout dataset, we simply choose the index such
+    # that there are always 1 episodes in the dataset.
+    "indices": np.array([]),
+}
+
+# This just adjusts the file name to include the number of timesteps (in 1000s).
+save_path = traj_path
+tmp_path = save_path + ".tmp"
+with open(tmp_path, "wb") as f:
+    np.savez_compressed(f, **condensed)
+
+os.replace(tmp_path, save_path)
+print(f"Saved trajectories to {save_path}.")
