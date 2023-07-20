6300219e3c5a100ba3a5acb1c012c6df129c78b8
Daniel Filan <df@danielfilan.com>
2023-07-19 20:43:46 -0700

    Train probes on list-valued attributes

--- src/reward_preprocessing/scripts/train_probe.py
+++ src/reward_preprocessing/scripts/train_probe.py
@@ -1,3 +1,5 @@
+import functools
+import operator
 import os
 import os.path
 from typing import List, Optional, Union
@@ -46,17 +48,45 @@ def train_probe(
     )
 
 
+def _sum_vecs(vec1, vec2):
+    return list(map(operator.add, vec1, vec2))
+
+
+def get_mean_attr_val(list_attr_vals):
+    num_vals = len(list_attr_vals)
+    if isinstance(list_attr_vals[0], list):
+        attr_sum = functools.reduce(_sum_vecs, list_attr_vals[1:], list_attr_vals[0])
+        norm_sum = [sum_val / num_vals for sum_val in attr_sum]
+        return norm_sum
+    else:
+        return sum(list_attr_vals) / num_vals
+
+
+def get_mse(list_attr_vals):
+    mean = get_mean_attr_val(list_attr_vals)
+
+    def get_mse_single_val(attr_val):
+        if isinstance(list_attr_vals[0], list):
+            mse = 0
+            for attr_scalar, mean_scalar in zip(attr_val, mean):
+                mse += (attr_scalar - mean_scalar) ** 2
+            return mse / len(list_attr_vals[0])
+        else:
+            return (attr_val - mean) ** 2
+
+    return sum(map(get_mse_single_val, list_attr_vals)) / len(list_attr_vals)
+
+
 @train_probe_ex.capture
 def benchmark_accuracy(dataset, use_next_info: bool, attributes: Union[str, List[str]]):
     """Determine the MSE from always guessing the mean value of the attributes."""
     attr_list = attributes if isinstance(attributes, list) else [attributes]
     mse = 0
     for attr in attr_list:
-        vec = list(
+        attr_vals = list(
             map(lambda x: x["next_infos" if use_next_info else "infos"][attr], dataset)
         )
-        mean = sum(vec) / len(vec)
-        attr_mse = sum(map(lambda x: (x - mean) ** 2, vec)) / len(vec)
+        attr_mse = get_mse(attr_vals)
         mse += attr_mse
     print("\nLoss from predicting mean:", mse)
 
